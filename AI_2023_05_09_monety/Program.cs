1ded1698b5075414e8baf06d8a059b0d9de4c2f5
Patryk Czajkowski <308104@stud.umk.pl>
2023-05-15 22:43:58 +0200

    Monety

---
+++ AI_2023_05_09_monety/Program.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.ML.Probabilistic.Models;
+using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+
+        //var rng = new Random();      
+
+        //E
+        //true - orzeł
+        //false - reszka
+
+        bool[] data = new bool[100];
+            for (int i = 0; i < data.Length - data.Length / 3; i++)
+                data[i] = true;
+
+            for (int i = data.Length - data.Length / 3; i < data.Length; i++)
+                data[i] = false; ;
+
+        //P(H)
+            Variable<double> mean = Variable.Beta(1, 1);
+
+        //P(E | H)
+        for (int i = 0; i < data.Length; i++)
+        {
+            Variable<bool> x = Variable.Bernoulli(mean);
+            x.ObservedValue = data[i];
+        }
+
+        var engine = new InferenceEngine();
+        Console.WriteLine("mean: " + engine.Infer(mean));
+    }
+    }
+
