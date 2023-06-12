09189659dfae83c36e4fe30d0e624cb9f2ba435d
Patryk Czajkowski <74250436+Patix214@users.noreply.github.com>
2023-06-13 01:40:31 +0200

    Update Program.cs

--- AI_2023_05_09_monety/Program.cs
+++ AI_2023_05_09_monety/Program.cs
@@ -7,12 +7,11 @@ class Program
         static void Main(string[] args)
         {
 
-        //var rng = new Random();      
-
+        //var rng = new Random();     
         //E
         //true - orzeł
         //false - reszka
-
+        //inicjacja danych - w tym przypadku 2/3 danych to orły, a 1/3 to reszki
         bool[] data = new bool[100];
             for (int i = 0; i < data.Length - data.Length / 3; i++)
                 data[i] = true;
@@ -20,10 +19,10 @@ class Program
             for (int i = data.Length - data.Length / 3; i < data.Length; i++)
                 data[i] = false; ;
 
-        //P(H)
+        //P(H) - prawdopodobieństwo a priori (prawdopodobieństwo hipotezy, przed obserwacją nowych dowodów
             Variable<double> mean = Variable.Beta(1, 1);
 
-        //P(E | H)
+        //P(E | H) - prawdopodobieństwo obserwacji przy założeniu hipotezy
         for (int i = 0; i < data.Length; i++)
         {
             Variable<bool> x = Variable.Bernoulli(mean);
@@ -31,7 +30,9 @@ class Program
         }
 
         var engine = new InferenceEngine();
-        Console.WriteLine("mean: " + engine.Infer(mean));
+            
+        //P(H | E) - prawdopodobieństwo a posteriori (prawdopodobieństwo hipotezy w świetle zaobserwowanych dowodów)
+        Console.WriteLine("Inferred values: " + engine.Infer(mean));
     }
     }
 
